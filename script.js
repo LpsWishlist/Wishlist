b964433ea2c2b8dd1f56dd5161630aa7206bf602
LpsWishlist <panconjamonz02@gmail.com>
2026-09-09 14:10:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 
 const products = {
     lps: [
-        { id: 1, name: 'Cocker Spaniel #1', image: '/images/lps-01.jpg', description: 'Perro juguetón y leal' },
+        { id: 1, name: 'Cocker Spaniel #1', image: 'images/lps-01.jpg', description: 'Perro juguetón y leal' },
         { id: 2, name: 'Gato Persa #5', image: '🐈', description: 'Felino elegante y tranquilo' },
         { id: 3, name: 'Conejo #12', image: '🐰', description: 'Conejito adorable y suave' },
         { id: 4, name: 'Hámster #7', image: '🐹', description: 'Pequeño y energético' },
