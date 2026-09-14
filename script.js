7bd15da296ec3fdbf97a331886f4e443fda90781
LpsWishlist <panconjamonz02@gmail.com>
2026-09-14 13:22:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -145,7 +145,7 @@ function createProductCard(product) {
     
     card.innerHTML = `
         <div class="image-container">
-            <div class="image">${product.image}</div>
+            <img class="image" src="${product.image}" alt="${product.name}">
             <div class="shimmer-effect"></div>
         </div>
         <div class="card-content">
